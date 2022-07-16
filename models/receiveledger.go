65b5d20cc54fd8455408e9937847f57272e3cbfe
Parzival Yuenyang <krumii.it@gmail.com>
2022-07-16 15:06:14 +0700

    update referense

--- models/receiveledger.go
+++ models/receiveledger.go
@@ -11,14 +11,14 @@ type ReceiveLedger struct {
 	gorm.Model
 	ID         string    `gorm:"index;size:21" form:"id"`
 	WhsID      string    `gorm:"size:21" form:"whs_id"`
-	Whs        Whs       `gorm:"foreignKey:WhsID;references:ID"`
 	FactoryID  string    `gorm:"size:21" form:"factory_id"`
-	Factory    Factory   `gorm:"foreignKey:FactoryID;references:ID"`
 	RssGroupID string    `gorm:"size:21" form:"rss_group_id"`
-	RssGroup   RssGroup  `gorm:"foreignKey:RssGroupID;references:ID"`
 	IsActive   bool      `gorm:"null" form:"is_active" default:"false"`
 	CreatedAt  time.Time `form:"created_at" default:"now"`
 	UpdatedAt  time.Time `form:"updated_at" default:"now"`
+	Whs        Whs       `gorm:"foreignKey:WhsID;references:ID"`
+	Factory    Factory   `gorm:"foreignKey:FactoryID;references:ID"`
+	RssGroup   RssGroup  `gorm:"foreignKey:RssGroupID;references:ID"`
 }
 
 func (u *ReceiveLedger) BeforeCreate(tx *gorm.DB) (err error) {
